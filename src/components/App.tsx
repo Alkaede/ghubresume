8580b21e086c032c998a201690e299a112031bc3
Andrew Castillo <andrewcast32@gmail.com>
2022-07-18 19:02:25 -0700

    :)
    commit to main

---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../css/main.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello 👋</h1>
+        <h2>&#129302;I am Andrew Castillo, I'm a full-stack software engineer passionate on building functional interfaces and useful products.</h2>
+        <h3>Get in touch with me on LinkedIn</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
