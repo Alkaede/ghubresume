4a97fb225085ced55281375354bcd6c8cfa35760
Andrew Castillo <andrewcast32@gmail.com>
2022-09-13 16:36:43 -0700

    background and skill section
    Added sections for a background and skill section to be organized properly

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -9,7 +9,60 @@ function App() {
         <h2>&#129302;I am Andrew Castillo, I'm a full-stack software engineer passionate on building functional interfaces and useful products.</h2>
         <h3>Get in touch with me on LinkedIn</h3>
       </header>
+
+      <section className="section background">
+        <div className="section_title">Background</div>
+        <div className="section_content">
+          <p>I graduated a rigorous six-month long bootcamp, learning all from the very basics to advanced site structures, requiring not only determination but also the ability to critically think and problem solve.</p>
+          <p>My communication skills and problem solving allow me to be a valuable asset to any team that I am a part of. I provide the essentials when it comes to group thinking and finding the best solutions to the puzzle of making websites functional and appealing</p>
+          <p>If I'm not behind my keyboard typing up a storm, I'm usually thinking of new Magic the Gathering decks to brew, trying to further my learning in Japanese and German, or taking a breather taking in everything with my cat.</p>
+        </div>
+      </section>
+
+      <section className="section skills">
+        <div className="section_title">Skills</div>
+        <div className="section_content">
+          <div className="skills">
+            <div className='skills_category'>
+              <div className="skills_label">Languages</div>
+              <ul>
+                <li className='skills_category_item'>JavaScript (ES6)</li>
+                <li className='skills_category_item'>HTML</li>
+                <li className='skills_category_item'>CSS/SCSS</li>
+                <li className='skills_category_item'>Python</li>
+                <li className='skills_category_item'>SQL</li>
+              </ul>
+            </div>
+
+            <div className="skills_category">
+              <div className="skills_label">Frameworks</div>
+              <ul>
+                <li className="skills_category_item">React</li>
+                <li className="skills_category_item">NodeJS</li>
+                <li className="skills_category_item">Express</li>
+                <li className="skills_category_item">KnexJS</li>
+              </ul>
+            </div>
+
+            <div className="skills_category">
+              <div className="skills_label">Tools</div>
+              <ul>
+                <li className="skills_category_item">Git & Github</li>
+                <li className="skills_category_item">Postman</li>
+                <li className="skills_category_item">Insomnia</li>
+                <li className="skills_category_item">Bash</li>
+              </ul>
+            </div>
+
+
+          </div>
+        </div>
+      </section>
+
+
     </div>
+
+
   );
 }
 
